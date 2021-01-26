bda081985277a53c3e3e65bb93f931b004cb6112
Creadde <ersinalp61@gmail.com>
2021-01-26 23:39:19 +0300

    ilk_versiyon

---
+++ test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 5;
+            if (x>10)
+            {
+                x++;
+
+            }
+            else if (x<=5)
+            {
+                x = x % 5;
+            }
+            else
+            {
+                x = 1;
+            }
+
+
+
+            Console.WriteLine(x);
+
+
+            Console.Read();
+        }
+    }
+}
